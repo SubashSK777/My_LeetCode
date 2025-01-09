04a2977f7c81ecda03ad4922b23a0570431b578b
Subash Kumar <subashsk11831@gmail.com>
2025-01-09 21:39:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (48.07%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,29 +1,19 @@
-class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if len(str1) > len(str2):
-            base = str2
-            maxi = str1
-            mini = str2
-        else:
-            base = str1
-            maxi = str2
-            mini = str1
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        def find_gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        gcd_len = find_gcd(len(str1), len(str2))
+
+        return str1[:gcd_len]
         
-        nmax = len(maxi)
-        nmini = len(mini)
-        count = 0
-        while (len(base) > 0):
-            if count == 1 :
-                break
-            if len(base) == 1:
-                count += 1
-            if (nmax % len(base) == 0 and nmini % len(base) == 0):
-                remax = nmax // len(base)
-                remini = nmini // len(base)   
-                if (base * remax == maxi and base * remini == mini):
-                    return (base)
-                else:    
-                    base = base.removesuffix(base[-1])
-            else:    
-                base = base.removesuffix(base[-1])
-        return ""
