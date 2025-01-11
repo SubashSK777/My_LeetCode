8a7c55be1698b0e12f0ff02bfa5026546ecc4e8f
Subash Kumar <subashsk11831@gmail.com>
2025-01-11 13:46:11 +0530

    Time: 24 ms (96.04%), Space: 18.1 MB (34.36%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
@@ -2,18 +2,16 @@ from collections import Counter
 
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
-        
+        count = 0
         if len(s) < k:
             return False
-
         s_map = Counter(s)
-        count = 0
+
         for i in s_map:
             if s_map[i] % 2 != 0:
                 count += 1
 
-        if count > k:
+        if count > k: 
             return False
         else:
             return True
-        
