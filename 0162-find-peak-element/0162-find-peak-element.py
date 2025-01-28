e6fcfe559d0a65550c1960a44672d012dcdd9190
Subash Kumar <subashsk11831@gmail.com>
2025-01-28 14:08:07 +0530

    Time: 0 ms (100%), Space: 18.1 MB (13.06%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -3,17 +3,26 @@ class Solution:
         l  = 0
         r = len(nums) - 1
 
+        if len(nums) == 1:
+            return 0
+
+        if nums[0] > nums[1]:
+            return 0
+
+        if nums[r] > nums[r - 1]:
+            return r
+
         while l < r:
             m = (l + r) // 2
 
-            # if nums[m - 1] < nums[m] > nums[m + 1]:
-            #     return m
+            if nums[m - 1] < nums[m] > nums[m + 1]:
+                return m
 
             if nums[m] < nums[m + 1]:
                 l = m + 1
 
             else:
-                r = m 
+                r = m - 1
 
         return l
         
